acbc85394bee436350d8fad88b898eaf6ae7f254
kiho <kihonoluluhawaii@gmail.com>
2024-04-08 15:19:53 +0900

    ✨ feat: footer 생성

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import Header from '@components/layout/header/Header';
-
+import Footer from '@components/layout/footer/Footer';
 function Layout() {
+  
   return (
     <div>
       <Header />
       레이아웃
+      <Footer />
     </div>
   );
 }
