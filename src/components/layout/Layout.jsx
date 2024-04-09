cc5220dcf211b9c44f8c770a6ed6b7fe46fd5853
kiho <kihonoluluhawaii@gmail.com>
2024-04-09 09:11:22 +0900

    ✨ feat: Community Page 생성 및 초기작업

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import Header from '@components/layout/header/Header';
 import Footer from '@components/layout/footer/Footer';
+import Community from '@pages/community/Community';
 function Layout() {
   
   return (
     <div>
       <Header />
-      레이아웃
+      <Community />
       <Footer />
     </div>
   );
---
+++ src/pages/community/Community.jsx
@@ -0,0 +1,16 @@
+import CommunityForMe from "@pages/community/CommunityForMe";
+import FeedList from "./FeedList";
+import styled from "styled-components";
+
+
+
+function Community(){    
+    return (
+        <>
+            <FeedList />
+            <CommunityForMe />
+        </>
+    )
+}
+
+export default Community;
