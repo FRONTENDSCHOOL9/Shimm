e01b257cc3847a113951786f0db900e00cb0a00d
JWJung-99 <39busy@naver.com>
2024-04-15 11:08:16 +0900

    🐛 fix: user store 수정

--- src/zustand/user.mjs
+++ src/zustand/user.mjs
@@ -2,11 +2,16 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
 const useUserStore = create(
-  persist(set => ({
-    user: null,
-    setUser: obj => set({ user: obj }),
-    storage: createJSONStorage(() => sessionStorage),
-  })),
+  persist(
+    set => ({
+      user: null,
+      setUser: obj => set({ user: obj }),
+    }),
+    {
+      name: 'user',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
 );
 
 export default useUserStore;
