3ee5d5c79e747261df73cb1308e6c972499d5b98
JWJung-99 <39busy@naver.com>
2024-04-15 14:39:00 +0900

    ✅ update: 시간, 테마 상태관리 업데이트

--- src/zustand/themeSelection.mjs
+++ src/zustand/themeSelection.mjs
@@ -1,9 +1,18 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
-const useSelectedThemeStore = create(set => ({
-  selectedTheme: '',
-  selectedThemeSet: value => set({ selectedTheme: value }),
-}));
+const useSelectedThemeStore = create(
+  persist(
+    set => ({
+      selectedTheme: '',
+      selectedThemeSet: value => set({ selectedTheme: value }),
+    }),
+    {
+      name: 'theme',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
 
 const useIsThemeSelectedStore = create(set => ({
   isThemeSelected: false,
--- src/zustand/timeSelection.mjs
+++ src/zustand/timeSelection.mjs
@@ -1,9 +1,18 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
-const useSelectedTimeStore = create(set => ({
-  selectedTime: '',
-  selectedTimeSet: value => set({ selectedTime: value }),
-}));
+const useSelectedTimeStore = create(
+  persist(
+    set => ({
+      selectedTime: '',
+      selectedTimeSet: value => set({ selectedTime: value }),
+    }),
+    {
+      name: 'selectedTime',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
 
 const useIsTimeSelectedStore = create(set => ({
   isTimeSelected: false,
