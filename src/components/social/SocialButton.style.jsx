41cd7c3e9688a16e3b98b4b0dfed5bc50bd6852a
Dain <danggin.min@gmail.com>
2024-04-18 17:42:38 +0900

    🚧 cont: 구글로 로그인 작업중

--- src/components/social/SocialButton.style.jsx
+++ src/components/social/SocialButton.style.jsx
@@ -10,8 +10,14 @@ const bgColors = {
   green: '#03c75a',
 };
 
+const colors = {
+  white: '#333',
+  yellow: '#3c1e1e',
+  green: '#fff'
+}
+
 const hoverColors = {
-  white: '#cccccc',
+  white: '#e6e6e6',
   yellow: '#c6b300',
   green: '#019d47',
 };
@@ -28,6 +34,12 @@ const icons = {
   green: iconNaver,
 }
 
+const borders = {
+  white: '#b4b4b4',
+  yellow: 'none',
+  green: 'none'
+}
+
 
 
 export const SocialButton = styled.button`
@@ -36,9 +48,10 @@ export const SocialButton = styled.button`
   height: 50px;
   background-color: ${props => bgColors[props.$bg]};
   border-radius: 25px;
-  border: ${props => (props.bgColors === 'white' ? '1px solid #333' : 'none')};
+  border: 1px solid transparent;
+  border-color: ${props => borders[props.$bg]};;
   text-align: center;
-  color: ${props => (props.bgColors === 'white' ? '#333' : '#fff')};
+  color: ${props => colors[props.$bg]};
   font-size: 1.4rem;
   font-weight: 200;
   transition: 0.3s ease-in-out;
@@ -48,6 +61,7 @@ export const SocialButton = styled.button`
   background-size: 20px auto;
   padding-left: 10px;
   box-sizing: border-box;
+  display: block;
 
   &:focus {
     box-shadow: inset 0 0 0 2px ${props => shadowColors[props.$bg]};
