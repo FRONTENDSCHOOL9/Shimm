2a0202ff168e437429f0087e74ad307a61caf5ba
Dain <danggin.min@gmail.com>
2024-04-18 10:35:23 +0900

    💄 style: input component 디자인 추가

--- src/components/input/Input.style.jsx
+++ src/components/input/Input.style.jsx
@@ -1,7 +1,34 @@
 
 import styled from "styled-components";
 
+const size = {
+  small: {
+    width: '90px',
+    height: '40px',
+  },
+  medium: {
+    width: '130px',
+    height: '50px',
+  },
+  full: {
+    width: '100%',
+    height: '50px',
+  },
+}
 
 
+export const CommonInput = styled.input`
+  width: ${props => size[props.size]?.width};
+  height: ${props => size[props.size]?.height};
+  border-radius: 6px;
+  border: 1px solid #CBCDCC;
+  background-color: #fff;
 
-export const CommonInput = styled.input``
+  &:focus {
+    box-shadow: inset 0 0 0 2px #55A25A;
+  }
+
+  @media (min-width: 740) {
+    max-width: 450px;
+  }
+`
