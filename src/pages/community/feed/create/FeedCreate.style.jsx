8af1b79d3b5dc5bd4bdd9672c8c8a3c381c20e55
dain <danggin.min@gmail.com>
2024-04-25 03:31:13 +0900

    💄 style: 게시글 등록 버튼 위치 이동

--- src/pages/community/feed/create/FeedCreate.style.jsx
+++ src/pages/community/feed/create/FeedCreate.style.jsx
@@ -6,15 +6,19 @@ export const CircleButton = styled.button`
   border-radius: 50%;
   background-color: #e6e6e6;
   position: fixed;
-  bottom: 10%;
-  right: 10%;
-  transform: translate(-50%, 50%);
+  bottom: 5%;
+  right: 35%;
+  transform: translate(-50%);
   font-size: 4rem;
   display: flex;
   align-items: center;
   justify-content: center;
   opacity: 0.8;
   transition: 0.3s ease-in-out;
+
+  @media (min-width: 800px) {
+    right: 20%;
+  }
 
   &:hover {
     background: #cbcbcb;
