b0a473429fe31d41eb41afbbadfa6a4611aeb920
kiho <kihonoluluhawaii@gmail.com>
2024-05-08 18:00:08 +0900

    ♻️ refactor: Calendar.jsx library없이 재구성, 디자인 및 CSS 수정필요

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,114 +1,204 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { Calendar, Whisper, Popover, Badge } from 'rsuite';
-import 'rsuite/dist/rsuite.min.css';
-import useCustomAxios from '@hooks/useCustomAxios.mjs';
+import React from 'react';
 import {
-  StyleGetRecord,
-  TodoItemBadge,
-  RecordList,
+  Calendar,
+  Container,
+  Day,
+  DayNumber,
+  EventName,
+  Header,
 } from '@components/calendar/Calendar.style';
+import { useState, useEffect } from 'react';
+import useCustomAxios from '@hooks/useCustomAxios.mjs';
+import useUserStore from '@zustand/user.mjs';
 
-const MyCalendar = () => {
+function MyCalendar() {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const today = new Date();
+  const [currentMonth, setCurrentMonth] = useState(today.getMonth());
+  const [currentYear, setCurrentYear] = useState(today.getFullYear());
   const [events, setEvents] = useState([]);
   const axios = useCustomAxios();
+  const { user } = useUserStore();
+
   useEffect(() => {
-    async function fetchEvents() {
-      try {
-        const res = await axios.get(
-          `/posts?type=meditation&year=${year}&month=${month}`,
-        );
-        if (res.data && res.data.item) {
-          const EventsRes = res.data.item.map(item => ({
+    fetchEvents();
+  }, [currentMonth, currentYear]);
+
+  async function fetchEvents() {
+    try {
+      const res = await axios.get(
+        `/posts?type=meditation&year=${currentYear}&month=${currentMonth + 1}`,
+      );
+      if (res.data && res.data.item) {
+        const EventsRes = res.data.item
+          .filter(item => item.user._id === user._id)
+          .map(item => ({
             time: item.updatedAt,
             title: item.content,
           }));
-          setEvents(EventsRes);
-        }
-        console.log(res);
-      } catch (err) {
-        console.error(err);
+        setEvents(EventsRes);
       }
+    } catch (err) {
+      console.error(err);
     }
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = today.getMonth() + 1;
-    fetchEvents(today);
-  }, []);
-
-  function parseUpdatedAt(updatedAt) {
-    const dateObj = new Date(updatedAt);
-    const formattedTime = `${padZero(dateObj.getHours())}:${padZero(dateObj.getMinutes())}`;
-    return formattedTime;
   }
 
-  function padZero(num) {
-    return num.toString().padStart(2, '0');
-  }
-
-  function getMeditation(date) {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-
-    const filteredEvents = events.filter(event => {
-      const eventDate = new Date(event.time);
-      return (
-        eventDate.getFullYear() === year &&
-        eventDate.getMonth() + 1 === month &&
-        eventDate.getDate() === day
+  function drawBlankCalendar() {
+    const blankDays = [];
+    for (let i = 0; i < 35; i++) {
+      blankDays.push(
+        <div key={i} className="day">
+          <p className="day-text"></p>
+          <p className="day-number"></p>
+          <small className="event-name"></small>
+        </div>,
       );
+    }
+    return blankDays;
+  }
+
+  function groupEventsByMonth(events) {
+    const eventsByMonth = {};
+    events.forEach(event => {
+      const eventDate = new Date(event.time);
+      const month = eventDate.getMonth();
+      const year = eventDate.getFullYear();
+      const key = `${year}-${month}`;
+
+      if (!eventsByMonth[key]) {
+        eventsByMonth[key] = [];
+      }
+      eventsByMonth[key].push(event);
+    });
+    return eventsByMonth;
+  }
+
+  function updateCalendar(month, year, events) {
+    const theFirst = new Date();
+    theFirst.setMonth(month);
+    theFirst.setYear(year);
+    const theFirstDayOfWeek = theFirst.getDay();
+    const monthName = months[month];
+    const monthWithYear = `${year} - ${monthName}`;
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const calendarDays = drawBlankCalendar();
+    let dayCounter = 1;
+
+    const updatedCalendarDays = calendarDays.map((day, i) => {
+      const dayNumberElement = day.props.children[1];
+      const dayTextElement = day.props.children[0];
+      const eventNameElement = day.props.children[2];
+
+      let dayNumber, dayText;
+
+      if (i >= theFirstDayOfWeek && dayCounter <= daysInMonth) {
+        dayNumber = <span key="dayNumber">{dayCounter}</span>;
+        dayCounter++;
+      } else {
+        dayNumber = <span key="dayNumber"></span>;
+      }
+
+      dayText = <span key="dayText">{days[i % 7]}</span>;
+
+      return React.cloneElement(day, {}, [
+        dayText,
+        dayNumber,
+        eventNameElement,
+      ]);
     });
 
-    if (filteredEvents.length > 0) {
-      return filteredEvents.map(event => ({
-        time: event.time,
-        title: event.title,
-      }));
-    } else {
-      return [];
-    }
+    return updatedCalendarDays;
   }
 
   function renderCell(date) {
-    const list = getMeditation(date);
-    const displayList = list.filter((item, index) => index < 2);
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const key = `${year}-${month}`;
 
-    if (list.length > 0) {
-      const moreCount = list.length - displayList.length;
-      const moreItem = (
-        <StyleGetRecord>
-          <Whisper
-            placement="top"
-            trigger="click"
-            speaker={
-              <Popover>
-                {list.map((item, index) => (
-                  <p key={index}>{item.title}</p>
-                ))}
-              </Popover>
-            }
-          >
-            <a>{moreCount} more</a>
-          </Whisper>
-        </StyleGetRecord>
-      );
-      return (
-        <RecordList>
-          {displayList.map((item, index) => (
-            <StyleGetRecord key={index}>
-              <TodoItemBadge />
-              {item.title}
-            </StyleGetRecord>
-          ))}
-          {moreCount ? moreItem : null}
-        </RecordList>
-      );
+    const eventsByMonth = groupEventsByMonth(events);
+    const eventsForDay = eventsByMonth[key]
+      ? eventsByMonth[key].filter(
+          event => new Date(event.time).getDate() === day,
+        )
+      : [];
+
+    if (eventsForDay.length > 0) {
+      return eventsForDay.map((event, index) => (
+        <EventName key={index} className="event-name">
+          {event.title}
+        </EventName>
+      ));
     }
+
     return null;
   }
+  function previousMonth() {
+    if (currentMonth === 0) {
+      setCurrentMonth(11);
+      setCurrentYear(currentYear - 1);
+    } else {
+      setCurrentMonth(currentMonth - 1);
+    }
+  }
 
-  return <Calendar bordered renderCell={renderCell} />;
-};
+  function nextMonth() {
+    if (currentMonth === 11) {
+      setCurrentMonth(0);
+      setCurrentYear(currentYear + 1);
+    } else {
+      setCurrentMonth(currentMonth + 1);
+    }
+  }
+
+  useEffect(() => {
+    updateCalendar(currentMonth, currentYear);
+  }, [currentMonth, currentYear]);
+
+  return (
+    <>
+      <Container>
+        <Header>
+          <button className="navigation" onClick={previousMonth}>
+            &lt;
+          </button>
+          <h1 id="month">{`${currentYear} - ${months[currentMonth]}`}</h1>
+          <button className="navigation" onClick={nextMonth}>
+            &gt;
+          </button>
+        </Header>
+        <Calendar>
+          {updateCalendar(currentMonth, currentYear).map((day, index) => (
+            <Day key={index}>
+              <p className="day-text">{day.props.children[0].props.children}</p>
+              {renderCell(
+                new Date(
+                  currentYear,
+                  currentMonth,
+                  day.props.children[1].props.children,
+                ),
+              )}
+              <DayNumber>{day.props.children[1].props.children}</DayNumber>
+            </Day>
+          ))}
+        </Calendar>
+      </Container>
+    </>
+  );
+}
 
 export default MyCalendar;
--- src/components/calendar/Calendar.style.jsx
+++ src/components/calendar/Calendar.style.jsx
@@ -2,37 +2,73 @@ import styled from 'styled-components';
 
 export const CalendarWrapper = styled.div`
   padding: 20px;
-  max-width: 640px;
+  max-width: 1000px;
   min-width: 440px;
   width: 100%;
+  height: 600px;
   display: flex;
   flex-direction: column;
   margin: 0 auto;
   box-sizing: border-box;
   align-items: center;
-  overflow-x: scroll;
+  gap: 30px;
+  /* overflow-x: auto; */
 `;
 
-export const RecordList = styled.ul`
-  text-align: left;
-  padding: 0;
-  list-style: none;
+export const Container = styled.div`
+  padding: 30px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: inset 0 0 20px red;
 `;
 
-export const StyleGetRecord = styled.li`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  color: #252121;
+export const Header = styled.div`
+  display: flex;
+  align-items: flex-start;
+  font-size: 1.4rem;
+  width: 140px;
+  margin-bottom: 10px;
+  justify-content: space-between;
+`;
 
-  & a {
-    color: gray;
+export const Calendar = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  width: 100%;
+  height: 100%;
+  max-width: 800px;
+  gap: 4px;
+`;
+
+export const Day = styled.div`
+  width: 100%;
+  height: 100%;
+  color: white;
+  background-color: #55a25a;
+  border-radius: 4px;
+  transition: all 0.5s;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &:hover {
+    background-color: #335635;
   }
 `;
 
-export const TodoItemBadge = styled.div`
-  width: 6px;
-  height: 6px;
-  background-color: #55a25a;
-  border-radius: 50%;
+export const DayNumber = styled.p`
+  font-size: 1.4rem;
+  font-weight: 300;
+  text-align: center;
+  margin: 0;
+`;
+
+export const EventName = styled.span`
+  font-size: 1.2rem;
+  color: #fefefe;
+  display: block;
 `;
